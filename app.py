c5b43b3b2dd660a447f00fc9c2a903b44c4b8241
Misha197408 <121133640+Misha197408@users.noreply.github.com>
2023-04-23 14:24:46 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import sklearn
+from sklearn.ensemble import RandomForestRegressor
+
+app = Flask(__name__, template_folder='template')
+model = pickle.load(open(r'C:\Users\55944\FLASK\vkr.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Прогнозируем прочность при растяжении {}'.format(output))
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
